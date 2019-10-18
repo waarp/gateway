151c56e0c1e511e9705a0ee7e00be77f7a6ff512
Paolo Pantellini <paolo.pantellini@waarp.org>
2019-10-18 12:09:31 +0200

    Added basic SFTP server

---
+++ pkg/sftp/handlers.go
@@ -0,0 +1,94 @@
+package sftp
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/sftp"
+)
+
+type fileWriterFunc func(r *sftp.Request) (io.WriterAt, error)
+
+func (fw fileWriterFunc) Filewrite(r *sftp.Request) (io.WriterAt, error) {
+	return fw(r)
+}
+
+type fileReaderFunc func(r *sftp.Request) (io.ReaderAt, error)
+
+func (fr fileReaderFunc) Fileread(r *sftp.Request) (io.ReaderAt, error) {
+	return fr(r)
+}
+
+type fileListerFunc func(r *sftp.Request) (sftp.ListerAt, error)
+
+func (fl fileListerFunc) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
+	return fl(r)
+}
+
+type listerAtFunc func(ls []os.FileInfo, offset int64) (int, error)
+
+func (la listerAtFunc) ListAt(ls []os.FileInfo, offset int64) (int, error) {
+	return la(ls, offset)
+}
+
+func makeHandlers() sftp.Handlers {
+	return sftp.Handlers{
+		FileGet:  makeFileReader(),
+		FilePut:  makeFileWriter(),
+		FileCmd:  nil,
+		FileList: makeFileLister(),
+	}
+}
+
+func makeFileReader() fileReaderFunc {
+	return func(r *sftp.Request) (io.ReaderAt, error) {
+		dir, _ := os.Getwd()
+		file, err := os.Open(dir + "/" + r.Filepath)
+		if err != nil {
+			return nil, err
+		}
+
+		return file, nil
+	}
+}
+
+func makeFileWriter() fileWriterFunc {
+	return func(r *sftp.Request) (io.WriterAt, error) {
+		dir, _ := os.Getwd()
+		file, err := os.Create(dir + "/" + r.Filepath)
+		if err != nil {
+			return nil, err
+		}
+
+		return file, nil
+	}
+}
+
+func makeFileLister() fileListerFunc {
+	listerAt := func(ls []os.FileInfo, offset int64) (int, error) {
+		dir, _ := os.Getwd()
+		infos := []os.FileInfo{}
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			infos = append(infos, info)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		var n int
+		if offset >= int64(len(infos)) {
+			return 0, io.EOF
+		}
+		n = copy(ls, infos[offset:])
+		if n < len(ls) {
+			return n, io.EOF
+		}
+		return n, nil
+	}
+
+	return func(r *sftp.Request) (sftp.ListerAt, error) {
+		return listerAtFunc(listerAt), nil
+	}
+}
---
+++ pkg/sftp/server.go
@@ -0,0 +1,199 @@
+package sftp
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/database"
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/log"
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/model"
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/tk/service"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/ssh"
+)
+
+// Server represents an instance of SFTP server.
+type Server struct {
+	Db     *database.Db
+	Config *model.LocalAgent
+
+	shutdown chan bool
+	logger   *log.Logger
+	state    service.State
+}
+
+func loadCert(db *database.Db, server *model.LocalAgent) (*model.Cert, error) {
+	cert := &model.Cert{OwnerType: server.TableName(), OwnerID: server.ID}
+	if err := db.Get(cert); err != nil {
+		if err == database.ErrNotFound {
+			return nil, fmt.Errorf("no certificate found for SFTP server '%s'",
+				server.Name)
+		}
+		return nil, err
+	}
+
+	return cert, nil
+}
+
+func loadSSHConfig(db *database.Db, cert *model.Cert) (*ssh.ServerConfig, error) {
+	config := &ssh.ServerConfig{
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			user := &model.LocalAccount{Login: c.User()}
+			if err := db.Get(user); err != nil {
+				return nil, err
+			}
+			if bcrypt.CompareHashAndPassword(user.Password, pass) != nil {
+				return nil, fmt.Errorf("authentication failed")
+			}
+
+			return &ssh.Permissions{}, nil
+		},
+	}
+
+	privateKey, err := ssh.ParsePrivateKey(cert.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	config.AddHostKey(privateKey)
+
+	return config, nil
+}
+
+func parseServerAddr(server *model.LocalAgent) (string, uint16, error) {
+	conf := map[string]interface{}{}
+
+	if err := json.Unmarshal(server.ProtoConfig, &conf); err != nil {
+		return "", 0, err
+	}
+
+	a := conf["address"]
+	p := conf["port"]
+
+	addr, ok := a.(string)
+	if !ok {
+		return "", 0, fmt.Errorf("invalid SFTP server address")
+	}
+
+	port, ok := p.(uint16)
+	if !ok {
+		return "", 0, fmt.Errorf("invalid SFTP server port")
+	}
+
+	return addr, port, nil
+}
+
+func (s *Server) listen(listener net.Listener, config *ssh.ServerConfig) {
+	var server *sftp.RequestServer
+	handlers := makeHandlers()
+
+	go func() {
+		<-s.shutdown
+		_ = listener.Close()
+		if server != nil {
+			_ = server.Close()
+		}
+	}()
+	for {
+		nConn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		_, channels, reqs, err := ssh.NewServerConn(nConn, config)
+		if err != nil {
+			continue
+		}
+
+		go ssh.DiscardRequests(reqs)
+
+		for newChannel := range channels {
+			if newChannel.ChannelType() != "session" {
+				_ = newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+				continue
+			}
+			channel, requests, err := newChannel.Accept()
+			if err != nil {
+				break
+			}
+
+			go func(in <-chan *ssh.Request) {
+				for req := range in {
+					ok := false
+					switch req.Type {
+					case "subsystem":
+						if string(req.Payload[4:]) == "sftp" {
+							ok = true
+						}
+					}
+					_ = req.Reply(ok, nil)
+				}
+			}(requests)
+
+			server = sftp.NewRequestServer(channel, handlers)
+			if err != nil {
+				break
+			}
+
+			if err := server.Serve(); err != nil && err != io.EOF {
+				break
+			}
+
+			_ = server.Close()
+		}
+	}
+}
+
+// Start starts the SFTP server.
+func (s *Server) Start() error {
+	start := func() error {
+		cert, err := loadCert(s.Db, s.Config)
+		if err != nil {
+			return err
+		}
+
+		sshConf, err := loadSSHConfig(s.Db, cert)
+		if err != nil {
+			return err
+		}
+
+		addr, port, err := parseServerAddr(s.Config)
+		if err != nil {
+			return err
+		}
+
+		listener, err := net.Listen("tcp", fmt.Sprintf("%s:%v", addr, port))
+		if err != nil {
+			return err
+		}
+
+		go s.listen(listener, sshConf)
+		return nil
+	}
+
+	s.logger = log.NewLogger(s.Config.Name)
+	s.state.Set(service.Starting, "")
+
+	if err := start(); err != nil {
+		s.state.Set(service.Error, err.Error())
+		return err
+	}
+
+	s.state.Set(service.Running, "")
+	return nil
+}
+
+// Stop stops the SFTP server.
+func (s *Server) Stop(ctx context.Context) error {
+	s.shutdown <- true
+	s.state.Set(service.Offline, "")
+	return nil
+}
+
+// State returns the state of the SFTP server.
+func (s *Server) State() *service.State {
+	return &s.state
+}
