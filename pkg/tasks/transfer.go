4045836ca542a58c7d98189de9ffcb050bb8be3d
Paolo Pantellini <paolo.pantellini@waarp.org>
2019-11-22 12:38:01 +0100

    Add TRANSFER task

---
+++ pkg/tasks/transfer.go
@@ -0,0 +1,109 @@
+package tasks
+
+import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/database"
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/model"
+)
+
+// TransferTask is a task which schedules a new transfer.
+type TransferTask struct{}
+
+func init() {
+	RunnableTasks["TRANSFER"] = &TransferTask{}
+}
+
+// Validate checks if the tasks has all the required arguments.
+func (t *TransferTask) Validate(task *model.Task) error {
+	var args map[string]interface{}
+	if err := json.Unmarshal(task.Args, &args); err != nil {
+		return err
+	}
+
+	if file, ok := args["file"].(string); !ok || file == "" {
+		return fmt.Errorf("missing transfer source file")
+	}
+	if to, ok := args["to"].(string); !ok || to == "" {
+		return fmt.Errorf("missing transfer remote partner")
+	}
+	if as, ok := args["as"].(string); !ok || as == "" {
+		return fmt.Errorf("missing transfer account")
+	}
+	if rule, ok := args["rule"].(string); !ok || rule == "" {
+		return fmt.Errorf("missing transfer rule")
+	}
+
+	return nil
+}
+
+func getTransferInfo(db *database.Db, args map[string]interface{}) (file string,
+	ruleID, agentID, accountID uint64, infoErr error) {
+
+	fileName, fileOK := args["file"].(string)
+	if !fileOK || fileName == "" {
+		infoErr = fmt.Errorf("missing transfer file")
+		return
+	}
+	file = fileName
+
+	ruleName, ruleOK := args["rule"].(string)
+	if !ruleOK || ruleName == "" {
+		infoErr = fmt.Errorf("missing transfer rule")
+		return
+	}
+	rule := &model.Rule{Name: ruleName}
+	if err := db.Get(rule); err != nil {
+		infoErr = fmt.Errorf("error getting rule: %s", err)
+		return
+	}
+	ruleID = rule.ID
+
+	agentName, agentOK := args["to"].(string)
+	if !agentOK || agentName == "" {
+		infoErr = fmt.Errorf("missing transfer remote partner")
+		return
+	}
+	agent := &model.RemoteAgent{Name: agentName}
+	if err := db.Get(agent); err != nil {
+		infoErr = fmt.Errorf("error getting partner: %s", err)
+		return
+	}
+	agentID = agent.ID
+
+	accName, accOK := args["as"].(string)
+	if !accOK || accName == "" {
+		infoErr = fmt.Errorf("missing transfer account")
+		return
+	}
+	acc := &model.RemoteAccount{RemoteAgentID: agent.ID, Login: accName}
+	if err := db.Get(acc); err != nil {
+		infoErr = fmt.Errorf("error getting account: %s", err)
+		return
+	}
+	accountID = acc.ID
+	return file, ruleID, agentID, accountID, nil
+}
+
+// Run executes the task by scheduling a new transfer with the given parameters.
+func (t *TransferTask) Run(args map[string]interface{}, processor *Processor) (string, error) {
+	file, ruleID, agentID, accID, err := getTransferInfo(processor.Db, args)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	trans := &model.Transfer{
+		RuleID:     ruleID,
+		IsServer:   false,
+		AgentID:    agentID,
+		AccountID:  accID,
+		SourcePath: file,
+		DestPath:   filepath.Base(file),
+	}
+	if err := processor.Db.Create(trans); err != nil {
+		return err.Error(), err
+	}
+	return "", nil
+}
---
+++ pkg/tasks/transfer_test.go
@@ -0,0 +1,210 @@
+package tasks
+
+import (
+	"encoding/json"
+	"testing"
+
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/database"
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/model"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTransferValidate(t *testing.T) {
+
+	Convey("Given a 'TRANSFER' task", t, func() {
+		trans := &TransferTask{}
+		args := map[string]interface{}{
+			"file": "/test/file",
+			"to":   "partner",
+			"as":   "account",
+			"rule": "transfer rule",
+		}
+
+		Convey("Given that the arguments are valid", func() {
+			b, err := json.Marshal(args)
+			So(err, ShouldBeNil)
+
+			task := &model.Task{
+				Args: b,
+			}
+
+			Convey("When validating the task", func() {
+				err := trans.Validate(task)
+
+				Convey("Then it should NOT return an error", func() {
+					So(err, ShouldBeNil)
+				})
+			})
+		})
+
+		Convey("Given that a parameter is NOT the valid type", func() {
+			args["file"] = true
+			b, err := json.Marshal(args)
+			So(err, ShouldBeNil)
+
+			task := &model.Task{
+				Args: b,
+			}
+
+			Convey("When validating the task", func() {
+				err := trans.Validate(task)
+
+				Convey("Then it should return an error", func() {
+					So(err, ShouldNotBeNil)
+				})
+			})
+		})
+
+		Convey("Given that a parameter is missing", func() {
+			delete(args, "to")
+			b, err := json.Marshal(args)
+			So(err, ShouldBeNil)
+
+			task := &model.Task{
+				Args: b,
+			}
+
+			Convey("When validating the task", func() {
+				err := trans.Validate(task)
+
+				Convey("Then it should return an error", func() {
+					So(err, ShouldNotBeNil)
+				})
+			})
+		})
+
+		Convey("Given that a parameter is empty", func() {
+			args["rule"] = ""
+			b, err := json.Marshal(args)
+			So(err, ShouldBeNil)
+
+			task := &model.Task{
+				Args: b,
+			}
+
+			Convey("When validating the task", func() {
+				err := trans.Validate(task)
+
+				Convey("Then it should return an error", func() {
+					So(err, ShouldNotBeNil)
+				})
+			})
+		})
+	})
+}
+
+func TestTransferRun(t *testing.T) {
+
+	Convey("Given a coherent database", t, func() {
+		db := database.GetTestDatabase()
+
+		rule := &model.Rule{
+			Name: "transfer rule",
+			Path: "/rule/path",
+		}
+		So(db.Create(rule), ShouldBeNil)
+
+		partner := &model.RemoteAgent{
+			Name:        "test partner",
+			Protocol:    "sftp",
+			ProtoConfig: []byte(`{"port":1,"address":"localhost","root":"/root"}`),
+		}
+		So(db.Create(partner), ShouldBeNil)
+
+		cert := &model.Cert{
+			OwnerType:   partner.TableName(),
+			OwnerID:     partner.ID,
+			Name:        "test cert",
+			PublicKey:   []byte("public key"),
+			Certificate: []byte("certificate"),
+		}
+		So(db.Create(cert), ShouldBeNil)
+
+		account := &model.RemoteAccount{
+			RemoteAgentID: partner.ID,
+			Login:         "test account",
+			Password:      []byte("password"),
+		}
+		So(db.Create(account), ShouldBeNil)
+
+		Convey("Given a 'TRANSFER' task", func() {
+			trans := &TransferTask{}
+			args := map[string]interface{}{
+				"file": "/test/file",
+				"to":   partner.Name,
+				"as":   account.Login,
+				"rule": rule.Name,
+			}
+			processor := &Processor{Db: db}
+
+			Convey("Given that the parameters are valid", func() {
+
+				Convey("When running the task", func() {
+					msg, err := trans.Run(args, processor)
+
+					Convey("Then it should NOT return an error", func() {
+						So(err, ShouldBeNil)
+
+						Convey("Then the log message should be empty", func() {
+							So(msg, ShouldBeEmpty)
+						})
+					})
+				})
+			})
+
+			Convey("Given that the partner does not exist", func() {
+				args["to"] = "toto"
+
+				Convey("When running the task", func() {
+					msg, err := trans.Run(args, processor)
+
+					Convey("Then it should return an error", func() {
+						So(err, ShouldNotBeNil)
+
+						Convey("Then the log message should say that the 'to' "+
+							"parameter is invalid", func() {
+							So(msg, ShouldEqual, "error getting partner: the "+
+								"record does not exist")
+						})
+					})
+				})
+			})
+
+			Convey("Given that the account does not exist", func() {
+				args["as"] = "toto"
+
+				Convey("When running the task", func() {
+					msg, err := trans.Run(args, processor)
+
+					Convey("Then it should return an error", func() {
+						So(err, ShouldNotBeNil)
+
+						Convey("Then the log message should say that the 'as' "+
+							"parameter is invalid", func() {
+							So(msg, ShouldEqual, "error getting account: the "+
+								"record does not exist")
+						})
+					})
+				})
+			})
+
+			Convey("Given that the rule does not exist", func() {
+				args["rule"] = "toto"
+
+				Convey("When running the task", func() {
+					msg, err := trans.Run(args, processor)
+
+					Convey("Then it should return an error", func() {
+						So(err, ShouldNotBeNil)
+
+						Convey("Then the log message should say that the 'rule' "+
+							"parameter is invalid", func() {
+							So(msg, ShouldEqual, "error getting rule: the "+
+								"record does not exist")
+						})
+					})
+				})
+			})
+		})
+	})
+}
