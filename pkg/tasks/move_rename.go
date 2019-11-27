9ec45ec107442ebfa2840819076cb5216f41b456
mro <maxime.robert@waarp.org>
2019-11-27 12:29:40 +0100

    add move_rename task

---
+++ pkg/tasks/move_rename.go
@@ -0,0 +1,47 @@
+package tasks
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/model"
+)
+
+// MoveRenameTask is a task which move and rename the current file
+// to a given destination
+type MoveRenameTask struct{}
+
+func init() {
+	RunnableTasks["MOVERENAME"] = &MoveRenameTask{}
+}
+
+// Validate check if the task has a destination for the move.
+func (*MoveRenameTask) Validate(t *model.Task) error {
+	args := map[string]interface{}{}
+	if err := json.Unmarshal(t.Args, &args); err != nil {
+		return err
+	}
+	if _, ok := args["path"]; !ok {
+		return fmt.Errorf("cannot create a move_rename task without a `path` argument")
+	}
+	return nil
+}
+
+// Run move and rename the current file to the destination and
+// modify the transfer model to reflect the file change.
+func (*MoveRenameTask) Run(args map[string]interface{}, processor *Processor) (string, error) {
+	newPath := args["path"].(string)
+	if processor.Rule.IsSend {
+		if err := os.Rename(processor.Transfer.SourcePath, newPath); err != nil {
+			return err.Error(), err
+		}
+		processor.Transfer.SourcePath = newPath
+		return "", nil
+	}
+	if err := os.Rename(processor.Transfer.DestPath, newPath); err != nil {
+		return err.Error(), err
+	}
+	processor.Transfer.DestPath = newPath
+	return "", nil
+}
