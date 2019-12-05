f7ff58fc9a4eaffdc295cdcf24a151fce3bb7506
Paolo Pantellini <paolo.pantellini@waarp.org>
2019-12-05 18:09:17 +0100

    Fixed executor transfer status & shutdown

--- pkg/executor/tasks_test.go
+++ pkg/executor/tasks_test.go
@@ -2,6 +2,7 @@ package executor
 
 import (
 	"fmt"
+	"time"
 
 	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/model"
 	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/tasks"
@@ -10,6 +11,7 @@ import (
 func init() {
 	tasks.RunnableTasks["TESTSUCCESS"] = &testTaskSuccess{}
 	tasks.RunnableTasks["TESTFAIL"] = &testTaskFail{}
+	tasks.RunnableTasks["TESTINFINITE"] = &testTaskInfinite{}
 }
 
 type testTaskSuccess struct{}
@@ -29,5 +31,17 @@ func (t *testTaskFail) Validate(*model.Task) error {
 }
 
 func (t *testTaskFail) Run(map[string]interface{}, *tasks.Processor) (string, error) {
+	return "task failed", fmt.Errorf("task failed")
+}
+
+type testTaskInfinite struct{}
+
+func (t *testTaskInfinite) Validate(*model.Task) error {
+	return nil
+}
+
+func (t *testTaskInfinite) Run(map[string]interface{}, *tasks.Processor) (string, error) {
+	for time.Now().Before(time.Now().Add(time.Minute)) {
+	}
 	return "task failed", fmt.Errorf("task failed")
 }
