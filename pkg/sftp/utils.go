1413d3f6eb5e36fb4945201852a0e17e1bc08adb
Paolo Pantellini <paolo.pantellini@waarp.org>
2020-03-04 16:08:21 +0100

    Fixed SFTP host key verification

--- pkg/sftp/utils.go
+++ pkg/sftp/utils.go
@@ -1,18 +1,57 @@
 package sftp
 
 import (
+	"bytes"
+	"fmt"
+	"net"
+
 	"code.waarp.fr/waarp-gateway/waarp-gateway/pkg/model"
 	"golang.org/x/crypto/ssh"
 )
 
-func getSSHConfig(c model.Cert, a *model.RemoteAccount) (*ssh.ClientConfig, error) {
-	key, _, _, _, err := ssh.ParseAuthorizedKey(c.PublicKey) //nolint:dogsled
-	if err != nil {
-		return nil, err
+type fixedHostKeys []ssh.PublicKey
+
+func (f fixedHostKeys) check(_ string, _ net.Addr, key ssh.PublicKey) error {
+	if len(f) == 0 {
+		return fmt.Errorf("ssh: required host key was nil")
 	}
+
+	remoteKey := key.Marshal()
+	for _, key := range f {
+		if bytes.Equal(remoteKey, key.Marshal()) {
+			return nil
+		}
+	}
+	return fmt.Errorf("ssh: host key mismatch")
+}
+
+func makeFixedHostKeys(keys []ssh.PublicKey) ssh.HostKeyCallback {
+	hk := fixedHostKeys(keys)
+	return hk.check
+}
+
+func getSSHConfig(certs []model.Cert, a *model.RemoteAccount) (*ssh.ClientConfig, error) {
 	pwd, err := model.DecryptPassword(a.Password)
 	if err != nil {
 		return nil, err
+	}
+
+	keys := []ssh.PublicKey{}
+	types := map[string]struct{}{}
+	for _, c := range certs {
+		key, _, _, _, err := ssh.ParseAuthorizedKey(c.PublicKey) //nolint:dogsled
+		if err != nil {
+			return nil, err
+		}
+
+		keys = append(keys, key)
+		types[key.Type()] = struct{}{}
+	}
+	algos := make([]string, len(types))
+	i := 0
+	for k := range types {
+		algos[i] = k
+		i++
 	}
 
 	return &ssh.ClientConfig{
@@ -20,6 +59,7 @@ func getSSHConfig(c model.Cert, a *model.RemoteAccount) (*ssh.ClientConfig, erro
 		Auth: []ssh.AuthMethod{
 			ssh.Password(string(pwd)),
 		},
-		HostKeyCallback: ssh.FixedHostKey(key),
+		HostKeyCallback:   makeFixedHostKeys(keys),
+		HostKeyAlgorithms: algos,
 	}, nil
 }
