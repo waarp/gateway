5b1449be77a543a09ed32b0581d912531a98ff32
mro <maxime.robert@waarp.org>
2020-05-26 15:22:54 +0200

    Add GWConf to ssh listener to prevent SIGSEV fault

--- pkg/sftp/service.go
+++ pkg/sftp/service.go
@@ -67,6 +67,7 @@ func (s *Service) Start() error {
 			ProtoConfig: &protoConfig,
 			SSHConf:     sshConf,
 			Listener:    listener,
+			GWConf:      s.db.Conf,
 		}
 		s.listener.ctx, s.listener.cancel = context.WithCancel(context.Background())
 		s.listener.listen()
