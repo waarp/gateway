1a490b6283cb1c2c36167a7cb0de55535a8a90eb
mro <maxime.robert@waarp.org>
2020-06-05 10:00:57 +0200

    better tests

--- pkg/tk/utils/filepath_test.go
+++ pkg/tk/utils/filepath_test.go
@@ -1,121 +1,41 @@
 package utils
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGetPath(t *testing.T) {
-	Convey("Given a non empty path", t, func() {
-		path := "ressource"
 
-		Convey("Given a list of non empty root paths", func() {
-			roots := []string{"in", "out/test"}
+	testCases := []struct {
+		path     string
+		roots    []string
+		expected string
+	}{
+		{"ressource", []string{"in", "out/test"}, "in/ressource"},
+		{"ressource", []string{"", "out/test"}, "out/test/ressource"},
+		{"ressource", []string{"", ""}, "ressource"},
+		{"ressource", []string{}, "ressource"},
+		{"/ressource", []string{"in", "out/test"}, "/ressource"},
+		{"/ressource", []string{"", "out/test"}, "/ressource"},
+		{"/ressource", []string{"", ""}, "/ressource"},
+		{"", []string{"in", "out/test"}, "in"},
+		{"", []string{"", "out/test"}, "out/test"},
+		{"", []string{"", ""}, ""},
+		{"", []string{}, ""},
+	}
 
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns 'in/ressource'", func() {
-					So(res, ShouldEqual, "in/ressource")
+	for _, tc := range testCases {
+		Convey(fmt.Sprintf("Given a ressource path: %s", tc.path), t, func() {
+			Convey(fmt.Sprintf("When calling the 'GetPath' function with roots: [%s]", strings.Join(tc.roots, ", ")), func() {
+				res := GetPath(tc.path, tc.roots...)
+				Convey(fmt.Sprintf("Then it should returns '%s'", tc.expected), func() {
+					So(res, ShouldEqual, tc.expected)
 				})
 			})
 		})
-
-		Convey("Given a list of root paths with empty first root", func() {
-			roots := []string{"", "out/test"}
-
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns 'in/ressource'", func() {
-					So(res, ShouldEqual, "out/test/ressource")
-				})
-			})
-		})
-
-		Convey("Given a list of empty root paths", func() {
-			roots := []string{"", ""}
-
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns 'in/ressource'", func() {
-					So(res, ShouldEqual, "ressource")
-				})
-			})
-		})
-
-		Convey("When calling the 'GetPath' function", func() {
-			res := GetPath(path)
-
-			Convey("Then it should returns ''", func() {
-				So(res, ShouldEqual, "ressource")
-			})
-		})
-	})
-
-	Convey("Given an empty path", t, func() {
-		path := ""
-
-		Convey("Given a list of non empty root paths", func() {
-			roots := []string{"in", "out/test"}
-
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns 'in/ressource'", func() {
-					So(res, ShouldEqual, "in")
-				})
-			})
-		})
-
-		Convey("Given a list of root paths with empty first root", func() {
-			roots := []string{"", "out/test"}
-
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns 'in/ressource'", func() {
-					So(res, ShouldEqual, "out/test")
-				})
-			})
-		})
-
-		Convey("Given a list of empty root paths", func() {
-			roots := []string{"", ""}
-
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns 'in/ressource'", func() {
-					So(res, ShouldEqual, "")
-				})
-			})
-		})
-
-		Convey("When calling the 'GetPath' function", func() {
-			res := GetPath(path)
-
-			Convey("Then it should returns ''", func() {
-				So(res, ShouldEqual, "")
-			})
-		})
-	})
-
-	Convey("Given a path starting with '/'", t, func() {
-		path := "/ressource"
-
-		Convey("Given a list of non empty root paths", func() {
-			roots := []string{"in", "out/test"}
-
-			Convey("When calling the 'GetPath' function", func() {
-				res := GetPath(path, roots...)
-
-				Convey("Then it should returns '/ressource'", func() {
-					So(res, ShouldEqual, "/ressource")
-				})
-			})
-		})
-	})
+	}
 }
