687acbedff9d8bb2ead2973eb0c9ee8428512f68
Paolo Pantellini <paolo.pantellini@waarp.org>
2020-09-01 13:38:07 +0200

    Changed database to prevent partial updates

--- pkg/database/database_hooks.go
+++ pkg/database/database_hooks.go
@@ -17,20 +17,31 @@ func InvalidError(msg string, args ...interface{}) *ErrInvalid {
 	return &ErrInvalid{Msg: fmt.Sprintf(msg, args...)}
 }
 
-// insertHook is an interface which adds a function called just before inserting
+// validator is an interface which adds a function called just before inserting
 // a new entry in the database.
-type insertHook interface {
-	BeforeInsert(Accessor) error
-}
-
-// updateHook is an interface which adds a function called just before updating
-// an entry in the database.
-type updateHook interface {
-	BeforeUpdate(Accessor, uint64) error
+type validator interface {
+	Validate(Accessor) error
 }
 
 // deleteHook is an interface which adds a function called just before deleting
 // an entry from the database.
 type deleteHook interface {
 	BeforeDelete(Accessor) error
+}
+
+// tableName is the interface that models MUST implement in order to have a
+// corresponding table in the database.
+type tableName interface {
+	TableName() string
+}
+
+// identifier is an interface which adds a function which returns the entry's
+// ID number. Models must implement this interface in order to be updated.
+type identifier interface {
+	Id() uint64
+}
+
+type entry interface {
+	tableName
+	identifier
 }
