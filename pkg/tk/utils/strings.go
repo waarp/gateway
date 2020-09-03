9553ca765600def890d59b0bc43aea8bb3510673
Paolo Pantellini <paolo.pantellini@waarp.org>
2020-09-03 11:31:15 +0200

    Added util functions to convert strings to/from string pointers

--- pkg/tk/utils/strings.go
+++ pkg/tk/utils/strings.go
@@ -15,3 +15,19 @@ func ContainsStrings(slice []string, strings ...string) bool {
 		return false
 	}) != n
 }
+
+// StringPtr takes a string and returns a pointer to that string. Useful for
+// putting a string literal where a string pointer is required.
+func StringPtr(s string) *string {
+	return &s
+}
+
+// String takes a string pointer and returns the string it is pointing to. If
+// the pointer is nil, returns an empty string. Useful to avoid panics bacause
+// of nil pointer dereference.
+func String(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
