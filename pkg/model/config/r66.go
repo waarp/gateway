8fe96ddb2c34e78a605cc9a6764639bd9ab9f33d
Paolo Pantellini <paolo.pantellini@waarp.org>
2020-10-09 12:30:45 +0200

    Updated R66Protoconfig for R66 integration

--- pkg/model/config/r66.go
+++ pkg/model/config/r66.go
@@ -1,15 +1,16 @@
 package config
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func init() {
 	ProtoConfigs["r66"] = func() ProtoConfig { return new(R66ProtoConfig) }
 }
 
 // R66ProtoConfig represents the configuration of a R66 agent.
-type R66ProtoConfig struct{}
+type R66ProtoConfig struct {
+	BlockSize      uint32 `json:"blockSize,omitempty"`
+	ServerPassword []byte `json:"serverPassword,omitempty"`
+}
 
 // ValidPartner checks if the configuration is valid for a R66 partner.
 func (c *R66ProtoConfig) ValidPartner() error {
@@ -18,5 +19,8 @@ func (c *R66ProtoConfig) ValidPartner() error {
 
 // ValidServer checks if the configuration is valid for a R66 server.
 func (c *R66ProtoConfig) ValidServer() error {
-	return fmt.Errorf("protocol unsuported")
+	if len(c.ServerPassword) == 0 {
+		return fmt.Errorf("missing server password")
+	}
+	return nil
 }
