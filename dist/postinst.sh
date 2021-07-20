2ed7bb9afafb3f83aab9db901632fcae3f4b3ba9
Bruno Carlin <bruno.carlin@waarp.org>
2021-07-20 18:12:28 +0200

    Fixes errors in post-install scripts

--- dist/postinst.sh
+++ dist/postinst.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Configuration # root:root 644
-chmod 755 /etc/waarp-gateway 
-chmod 644 /etc/waarp-gateway/waarp-gatewayd.ini
+chmod 755 /etc/waarp-gateway
+chmod 644 /etc/waarp-gateway/gatewayd.ini
 
 # Logs # root:waarp 664
-chown root:waarp /usr/share/waarp-gateway/* 
-chmod 664 /var/log/waarp-gateway/*
+chown root:waarp /var/log/waarp-gateway
+chmod 664 /var/log/waarp-gateway
 
 # Data # waarp:waarp 774
 chown waarp: /var/lib/waarp-gateway/
 chmod 774 /var/lib/waarp-gateway/
 
 # Share # root:waarp 750
-chown root:waarp /usr/share/waarp-gateway/* 
+chown root:waarp /usr/share/waarp-gateway/*
 chmod 750 /usr/share/waarp-gateway/*
 
 systemctl daemon-reload
