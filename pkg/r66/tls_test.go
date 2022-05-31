dbe362b5e08edc2c93d080d48bcb2b21acfedeb1
Paolo Pantellini <paolo.pantellini@waarp.org>
2022-05-31 16:51:41 +0200

    test: added some tests for the legacy R66 certificate validation

--- pkg/r66/tls_test.go
+++ pkg/r66/tls_test.go
@@ -58,9 +58,9 @@ func TestTLS(t *testing.T) {
 				model.IsLegacyR66CertificateAllowed = true
 				defer func() { model.IsLegacyR66CertificateAllowed = false }()
 
-				ctx.AddCryptos(c, cert(ctx.LocAccount, "loc_acc_cert", legacyR66Cert, ""))
+				ctx.AddCryptos(c, cert(ctx.LocAccount, "loc_acc_cert", testhelpers.LegacyR66Cert, ""))
 
-				err := tlsConnect(ctx, legacyR66Cert, legacyR66Key)
+				err := tlsConnect(ctx, testhelpers.LegacyR66Cert, testhelpers.LegacyR66Key)
 
 				Convey("Then it should not return an error", func(c C) {
 					So(err, ShouldBeNil)
@@ -71,7 +71,7 @@ func TestTLS(t *testing.T) {
 				model.IsLegacyR66CertificateAllowed = true
 				defer func() { model.IsLegacyR66CertificateAllowed = false }()
 
-				err := tlsConnect(ctx, legacyR66Cert, legacyR66Key)
+				err := tlsConnect(ctx, testhelpers.LegacyR66Cert, testhelpers.LegacyR66Key)
 
 				Convey("Then it should return an error", func(c C) {
 					r66Err := &r66.Error{}
@@ -81,7 +81,7 @@ func TestTLS(t *testing.T) {
 			})
 
 			Convey("Given that the legacy certificate is NOT allowed", func(c C) {
-				err := tlsConnect(ctx, legacyR66Cert, legacyR66Key)
+				err := tlsConnect(ctx, testhelpers.LegacyR66Cert, testhelpers.LegacyR66Key)
 
 				Convey("Then it should return an error", func(c C) {
 					r66Err := &r66.Error{}
@@ -131,8 +131,8 @@ func TestTLS(t *testing.T) {
 				model.IsLegacyR66CertificateAllowed = true
 				defer func() { model.IsLegacyR66CertificateAllowed = false }()
 
-				ctx.AddCryptos(c, cert(ctx.Partner, "partner_cert", legacyR66Cert, ""))
-				tlsServer(ctx, legacyR66Cert, legacyR66Key)
+				ctx.AddCryptos(c, cert(ctx.Partner, "partner_cert", testhelpers.LegacyR66Cert, ""))
+				tlsServer(ctx, testhelpers.LegacyR66Cert, testhelpers.LegacyR66Key)
 
 				Convey("Then it should not return an error", func(c C) {
 					So(getClient().connect(), ShouldBeNil)
@@ -143,7 +143,7 @@ func TestTLS(t *testing.T) {
 				model.IsLegacyR66CertificateAllowed = true
 				defer func() { model.IsLegacyR66CertificateAllowed = false }()
 
-				tlsServer(ctx, legacyR66Cert, legacyR66Key)
+				tlsServer(ctx, testhelpers.LegacyR66Cert, testhelpers.LegacyR66Key)
 
 				Convey("Then it should return an error", func(c C) {
 					So(getClient().connect(), ShouldBeError, types.NewTransferError(
@@ -152,7 +152,7 @@ func TestTLS(t *testing.T) {
 			})
 
 			Convey("Given that the legacy certificate is NOT allowed", func(c C) {
-				tlsServer(ctx, legacyR66Cert, legacyR66Key)
+				tlsServer(ctx, testhelpers.LegacyR66Cert, testhelpers.LegacyR66Key)
 
 				Convey("Then it should return an error", func(c C) {
 					So(getClient().connect(), ShouldBeError, types.NewTransferError(
@@ -243,54 +243,3 @@ func tlsConnect(ctx *pipelinetest.ServerContext, cert, key string) error {
 
 	return err //nolint:wrapcheck // this is a test
 }
-
-const legacyR66Cert = `-----BEGIN CERTIFICATE-----
-MIIDdzCCAl+gAwIBAgIENnEdtTANBgkqhkiG9w0BAQsFADBsMRAwDgYDVQQGEwdV
-bmtub3duMRAwDgYDVQQIEwdVbmtub3duMRAwDgYDVQQHEwdVbmtub3duMRAwDgYD
-VQQKEwdVbmtub3duMRAwDgYDVQQLEwdVbmtub3duMRAwDgYDVQQDEwdVbmtub3du
-MB4XDTEzMDQyOTA5NTQ1N1oXDTEzMDcyODA5NTQ1N1owbDEQMA4GA1UEBhMHVW5r
-bm93bjEQMA4GA1UECBMHVW5rbm93bjEQMA4GA1UEBxMHVW5rbm93bjEQMA4GA1UE
-ChMHVW5rbm93bjEQMA4GA1UECxMHVW5rbm93bjEQMA4GA1UEAxMHVW5rbm93bjCC
-ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAITDuqCocEqDFTVuHusQmb24
-L5haEN4tSRbULD9NHe0SehLU+3kXrSm97m6ffIbBj95ChocvMQhCpwQsfiTNa+pT
-dMTlwWN/jJEwAgfphqsDndoI+laGYJeeEhByxUaFQ608QXBUVigCdirz/T5cbkXl
-jmYWA9Rar259vefE6Eubfb/wS2kBKTbP96IqOH84R2Edsl45KM6tHVXh8/VynQdZ
-MVqJrMg5julPF1d0/Y/4UYoemOV+qaVrnawriZvg7+o8MLb1v7I7yok3lxpt/9TB
-Rs23OElpCzNlY7Zz3f0BD+lt8ZpoeXR7rN+1RMm0VwlIrr6Sske6211AtG3+qwEC
-AwEAAaMhMB8wHQYDVR0OBBYEFEVpbVKGEkgeXHQ0tN5lzLr36mVsMA0GCSqGSIb3
-DQEBCwUAA4IBAQBI17aBzzRZoQP8BSLTCPIJgApjoD0DWJ8TyKb76uLADcTVqtc4
-B1m7di0B6PT2dIT7+E4Ek0twOvfpUyPcgNUz0auAfBF27PJMSu2hug9HdSndvFBx
-aDANCVj1H7S+QgQpxQXNs6d9mwfpyOS/SCKE7Xy26/kKbQ1oWop2gV7w/+0LHK9A
-AdsPDkoTxPZFcPS7kZwg+eAov+DEOksOkWeHGypdFEsn4RqT67RheCeOZsiED4zh
-ADwdXdxu2+QYlSw3p/vuriC6FEWJ4E7MPMbFTUbax3Zx7ejvs8e/DgfjZZHkwkFd
-SRJz9CF60Oo+fqyp/TvfM/p3So5W5kAs9MLs
------END CERTIFICATE-----`
-
-const legacyR66Key = `-----BEGIN PRIVATE KEY-----
-MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCEw7qgqHBKgxU1
-bh7rEJm9uC+YWhDeLUkW1Cw/TR3tEnoS1Pt5F60pve5un3yGwY/eQoaHLzEIQqcE
-LH4kzWvqU3TE5cFjf4yRMAIH6YarA53aCPpWhmCXnhIQcsVGhUOtPEFwVFYoAnYq
-8/0+XG5F5Y5mFgPUWq9ufb3nxOhLm32/8EtpASk2z/eiKjh/OEdhHbJeOSjOrR1V
-4fP1cp0HWTFaiazIOY7pTxdXdP2P+FGKHpjlfqmla52sK4mb4O/qPDC29b+yO8qJ
-N5cabf/UwUbNtzhJaQszZWO2c939AQ/pbfGaaHl0e6zftUTJtFcJSK6+krJHuttd
-QLRt/qsBAgMBAAECggEAE9avj4w741Z9F9PRuOxtHMVmD0z+EkUQE+I2jmr2mtNU
-/HVo8mpQTNl9xHf+gqBv4BVuxsqNeB+Fl4EShGtRwd0gqL9wS27m0VcsJoSFxA4x
-S0BmMAG6c02Cg4Sy59vIBh3n5WIk0au0fqyg3e2v6K/pvGVzwwqeBlOxye1JjOqD
-G3aL2UefVjxPgLLE1mDoqV5ZIN2+XRXGFHJlvhA50RVDq1KQldFcbWrVTZf+Igi7
-XFLR+hIOFoZmLku2BHxXBjZRJO7REV8HbT/zIHi0iFv7IK/x+66r/wL8rLiwFGeK
-yA61EF0jPECgOxXURTZgTxhDwC9QPDmNSdgM1F1IBQKBgQC+Gtrc0P0fOQjehgyP
-4sHhvO/2BUKGUmi5c7QawE/ja2ueefosmGRU87l3bV4x2+GrR9yX5ymv08bVtJwC
-u/yncnyx6mjkMaiNXBtfrdNhKWN4GQJDF2GNur+hpXNvtBmlvulSBngbCwPrxjKa
-daflVYbADyreaO7iXMUgWjJZrwKBgQCyyLkem0Vm39r44Knxq/iGx/CAD3vsGnGI
-FUx0a+bxhFIKYQm9MLJtGN5Ag6kP+76snBLxJ6JSwxIBpG9JYrFLaEN49oiswcty
-mfO2zIUoZ8CHnFdoR0POXDTWLTLPWCd0ogxzDsVTKT4gavA9WErvFr0twIAMqS/Y
-LzbV9+BiTwKBgH2tR0+AIjbH/+MMf7WH1WElBQaCB67BQFaJ9WFSDf5s/6KvRQLC
-ZGH9FnmrpgAUOyZ+xYju25JP0T1qv1DXcnpIp8L/EwT5B1Mct0QTqJCtSgMVlXdB
-N874zMNSm/QW/nWitqDxgelu6NKwHrgaXDqyxfimjlKm0HZ5miB/QJYlAoGAEyid
-ZeE/w7Fzdr4kmAhUvqTIagC+x+NhjTKzGbrCadlDLWeOsp54UGac0o8JW/QfT8H9
-6afUpkfPMyva3SNdWnZW3KyWouS1l5dV3Z33GwhbQm0HlN4mLwQEiXsYec25lK8U
-5HONw8akqLas/fXrOcnXBgMd9b1fqiwNFUrV2dMCgYAnRZ7Ig3w+pkc5dAV22SNO
-4M3JJYqCiGBoGJR/w5IP1FgT+IshA/5fIBJl7s8Cg8aaWWoRYuLLjA1xTFqw+Ma9
-wvThKXCE78uQIzRIyp9X6W+enbMKesrtprpsZlBHU/lZ5m/bh3EXBuCFV1Q2rrVc
-5VAeza4keDveGJVWVTdTlw==
------END PRIVATE KEY-----`
