974e61e5c7881a00c83c9abf7a26547dc242fc19
Paolo Pantellini <paolo.pantellini@waarp.org>
2025-03-06 15:02:15 +0100

    refactor: Exported protocols list

--- pkg/protocols/modules.go
+++ pkg/protocols/modules.go
@@ -9,10 +9,10 @@ import (
 )
 
 // Register registers a new protocol module.
-func Register(name string, p Module) { list[name] = p }
+func Register(name string, p Module) { List[name] = p }
 
 // Get returns the protocol implementation with the given name.
-func Get(name string) Module { return list[name] }
+func Get(name string) Module { return List[name] }
 
 // IsValid returns whether the given protocol is implemented.
 func IsValid(name string) bool { return Get(name) != nil }
@@ -20,7 +20,7 @@ func IsValid(name string) bool { return Get(name) != nil }
 // List is the list of all protocols implemented by the gateway .
 //
 //nolint:gochecknoglobals //global var is required here
-var list = map[string]Module{
+var List = map[string]Module{
 	sftp.SFTP:      &sftp.Module{},      // SFTP
 	r66.R66:        &r66.Module{},       // R66
 	r66.R66TLS:     &r66.ModuleTLS{},    // R66-TLS
