f26d94dc5e3c3b5cb7178822df12005386d39b2e
Rayan Mazri <rayanmazri@Rayans-MacBook-Pro.local>
2025-05-12 15:27:59 +0200

    feat/add map to assign value in html (for exemple title of page home is Accueil)

--- pkg/admin/gui/homepage.go
+++ pkg/admin/gui/homepage.go
@@ -10,7 +10,7 @@ import (
 
 func homepage(db *database.DB, logger *log.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-        if err := templates.ExecuteTemplate(w, "home_page.html", nil); err != nil {
+        if err := templates.ExecuteTemplate(w, "home_page", map[string]any{"Title": "Accueil"}); err != nil {
             logger.Error("render homepage: %v", err)
             http.Error(w, "Erreur interne", http.StatusInternalServerError)
         }
