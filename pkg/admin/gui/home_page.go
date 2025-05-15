2c14796f4c387a34b2a04a8d8895c105666bb435
Rayan Mazri <rayanmazri@Rayans-MacBook-Pro.local>
2025-05-15 10:30:59 +0200

    fix/linter error

--- pkg/admin/gui/home_page.go
+++ pkg/admin/gui/home_page.go
@@ -6,7 +6,7 @@ import (
 	"code.waarp.fr/lib/log"
 )
 
-func home_page(logger *log.Logger) http.HandlerFunc {
+func homePage(logger *log.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := templates.ExecuteTemplate(w, "home_page", map[string]any{"Title": "Accueil"}); err != nil {
 			logger.Error("render home_page: %v", err)
