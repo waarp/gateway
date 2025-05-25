64bf499b8c4008aec41cb135cb6325d8b21ba2c7
Rayan Mazri <rayan.mazri@waarp.org>
2025-05-26 01:23:31 +0200

    feat/add parsing json file, store in  map containing the translations of the pages(id page -> element html -> language -> textTranslated)

--- pkg/admin/gui/assets.go
+++ pkg/admin/gui/assets.go
@@ -5,4 +5,6 @@ import "embed"
 //go:embed front_end/html/*.html
 //go:embed front_end/css/*.css
 //go:embed front_end/assets/*.png
+//go:embed front_end/multi_language/*.json
+
 var webFS embed.FS
--- pkg/admin/gui/multi_language.go
+++ pkg/admin/gui/multi_language.go
@@ -1,11 +1,49 @@
 package gui
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 const hoursDay = 24
+
+//nolint:gochecknoglobals // map
+var mapLanguage = make(map[string]map[string]map[string]string)
+
+//nolint:gochecknoinits // init
+func init() {
+	entries, err := webFS.ReadDir("front_end/multi_language")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		file, err := webFS.ReadFile("front_end/multi_language/" + e.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		page := make(map[string]map[string]string)
+		if err := json.Unmarshal(file, &page); err != nil {
+			log.Fatal(err)
+		}
+		id := strings.TrimSuffix(e.Name(), ".json")
+		mapLanguage[id] = page
+	}
+}
+
+func pageTranslated(id, userLanguage string) map[string]string {
+	tabTranslated := make(map[string]string)
+
+	for key, translation := range mapLanguage[id] {
+		tabTranslated[key] = translation[userLanguage]
+	}
+
+	return tabTranslated
+}
 
 func changeLanguage(w http.ResponseWriter, r *http.Request) string {
 	userLanguage := r.URL.Query().Get("language")
