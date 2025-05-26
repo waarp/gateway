60da42e0454b2cabbc12a54daaea0d2b539a30c0
Rayan Mazri <rayan.mazri@waarp.org>
2025-05-26 14:20:12 +0200

    feat/add dynamically add elements in home html

--- pkg/admin/gui/home_page.go
+++ pkg/admin/gui/home_page.go
@@ -4,19 +4,27 @@ import (
 	"net/http"
 
 	"code.waarp.fr/lib/log"
+
+	"code.waarp.fr/apps/gateway/gateway/pkg/database"
 )
 
-func homePage(logger *log.Logger) http.HandlerFunc {
+func homePage(logger *log.Logger, db *database.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		userLanguage := r.Context().Value(ContextLanguageKey)
 		tabTranslated := pageTranslated("home_page", userLanguage.(string)) //nolint:errcheck,forcetypeassert // userLanguage
 
+		user, err := GetUserByToken(r, db)
+		if err != nil {
+			logger.Error("Internal error loading user session: %v", err)
+		}
+
 		if err := templates.ExecuteTemplate(w, "home_page", map[string]any{
 			"tab":      tabTranslated,
+			"username": user.Username,
 			"language": userLanguage,
 		}); err != nil {
 			logger.Error("render home_page: %v", err)
-			http.Error(w, "Erreur interne", http.StatusInternalServerError)
+			http.Error(w, "Internal error", http.StatusInternalServerError)
 		}
 	}
 }
