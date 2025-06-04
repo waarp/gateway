66e0ca47bb21d798a52ff15ea813032ac4aeac92
Rayan Mazri <rayan.mazri@waarp.org>
2025-06-04 21:45:01 +0200

    feat/add version of page user management functional

--- pkg/admin/gui/user_management_page.go
+++ pkg/admin/gui/user_management_page.go
@@ -2,6 +2,7 @@
 package gui
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -14,7 +15,109 @@ import (
 
 type userPermissions struct {
 	Username    string
+	ID          int64
 	Permissions *model.Permissions
+}
+
+func addUser(db *database.DB, r *http.Request) error {
+	var newUser model.User
+	var permissions model.Permissions
+	var err error
+
+	newUserUsername := r.URL.Query().Get("newUserUsername")
+	if newUserUsername != "" {
+		newUser.Username = newUserUsername
+	}
+
+	newUserPassword := r.URL.Query().Get("newUserPassword")
+	if newUserPassword != "" {
+		newUser.PasswordHash, err = internal.HashPassword(newUserPassword)
+		if err != nil {
+			return fmt.Errorf("internal error: %w", err)
+		}
+	}
+	permissions.Transfers = r.URL.Query().Get("newUserPermissionsTransfers")
+	permissions.Servers = r.URL.Query().Get("newUserPermissionServers")
+	permissions.Partners = r.URL.Query().Get("newUserPermissionsPartners")
+	permissions.Rules = r.URL.Query().Get("newUserPermissionsRules")
+	permissions.Users = r.URL.Query().Get("newUserPermissionsUsers")
+	permissions.Administration = r.URL.Query().Get("newUserPermissionsAdministration")
+
+	newUser.Permissions, err = model.PermsToMask(&permissions)
+	if err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	if err = internal.InsertUser(db, &newUser); err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	return nil
+}
+
+func editUser(db *database.DB, r *http.Request) error {
+	var permissions model.Permissions
+	userID := r.URL.Query().Get("editUserID")
+
+	id, err := strconv.Atoi(userID)
+	if err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	newUser, err := internal.GetUserByID(db, int64(id))
+	if err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	newUserUsername := r.URL.Query().Get("editUserUsername")
+	if newUserUsername != "" {
+		newUser.Username = newUserUsername
+	}
+
+	newUserPassword := r.URL.Query().Get("editUserPassword")
+	if newUserPassword != "" {
+		newUser.PasswordHash, err = internal.HashPassword(newUserPassword)
+		if err != nil {
+			return fmt.Errorf("internal error: %w", err)
+		}
+	}
+	permissions.Transfers = r.URL.Query().Get("editUserPermissionsTransfers")
+	permissions.Servers = r.URL.Query().Get("editUserPermissionsServers")
+	permissions.Partners = r.URL.Query().Get("editUserPermissionsPartners")
+	permissions.Rules = r.URL.Query().Get("editUserPermissionsRules")
+	permissions.Users = r.URL.Query().Get("editUserPermissionsUsers")
+	permissions.Administration = r.URL.Query().Get("editUserPermissionsAdministration")
+
+	newUser.Permissions, err = model.PermsToMask(&permissions)
+	if err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	if err = internal.UpdateUser(db, newUser); err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	return nil
+}
+
+func deleteUser(db *database.DB, r *http.Request) error {
+	userID := r.URL.Query().Get("deleteUser")
+
+	id, err := strconv.Atoi(userID)
+	if err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	newUser, err := internal.GetUserByID(db, int64(id))
+	if err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	if err = internal.DeleteUser(db, newUser); err != nil {
+		return fmt.Errorf("internal error: %w", err)
+	}
+
+	return nil
 }
 
 func listUser(db *database.DB, r *http.Request) []*model.User {
@@ -43,6 +146,7 @@ func listUser(db *database.DB, r *http.Request) []*model.User {
 	return user
 }
 
+//nolint:funlen // pattern
 func userManagementPage(logger *log.Logger, db *database.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		userLanguage := r.Context().Value(ContextLanguageKey)
@@ -53,16 +157,50 @@ func userManagementPage(logger *log.Logger, db *database.DB) http.HandlerFunc {
 		for _, u := range userList {
 			uPermissionsList = append(uPermissionsList, userPermissions{
 				Username:    u.Username,
+				ID:          u.ID,
 				Permissions: model.MaskToPerms(u.Permissions),
 			})
 		}
 
-		user, err := GetUserByToken(r, db)
-		myPermission := model.MaskToPerms(user.Permissions)
+		if r.Method == http.MethodGet && r.URL.Query().Get("newUserUsername") != "" {
+			newUserErr := addUser(db, r)
+			if newUserErr != nil {
+				logger.Error("Internal error: %v", newUserErr)
+			}
 
+			http.Redirect(w, r, r.URL.Path, http.StatusSeeOther)
+
+			return
+		}
+
+		if r.Method == http.MethodGet && r.URL.Query().Get("editUserID") != "" {
+			editUserErr := editUser(db, r)
+			if editUserErr != nil {
+				logger.Error("Internal error: %v", editUserErr)
+			}
+
+			http.Redirect(w, r, r.URL.Path, http.StatusSeeOther)
+
+			return
+		}
+
+		if r.Method == http.MethodGet && r.URL.Query().Get("deleteUser") != "" {
+			deleteUserErr := deleteUser(db, r)
+			if deleteUserErr != nil {
+				logger.Error("Internal error: %v", deleteUserErr)
+			}
+
+			http.Redirect(w, r, r.URL.Path, http.StatusSeeOther)
+
+			return
+		}
+
+		user, err := GetUserByToken(r, db)
 		if err != nil {
 			logger.Error("Internal error: %v", err)
 		}
+
+		myPermission := model.MaskToPerms(user.Permissions)
 
 		if err := userManagementTemplate.ExecuteTemplate(w, "user_management_page", map[string]any{
 			"userPermissions": uPermissionsList,
