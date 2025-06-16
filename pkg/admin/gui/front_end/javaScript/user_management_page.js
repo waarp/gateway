de191c2d7c38a3c92dc9416eb439f887a9835ae1
Rayan Mazri <rayan.mazri@waarp.org>
2025-06-16 16:00:22 +0200

    feat/add feature for UX (eye button to see/hide a password, sub-page relative to the user's limit (30 by default) and auto-completion of the search bar to see those available in the database)

--- pkg/admin/gui/front_end/javaScript/user_management_page.js
+++ pkg/admin/gui/front_end/javaScript/user_management_page.js
@@ -38,7 +38,53 @@ function confirmPassword(password, passwordConfirm, language) {
     }
 }
 
+function eyePassword(id, btn) {
+    const password = document.getElementById(id);
+
+    password.type = password.type === "password" ? "text" : "password"
+
+    const icon = btn.querySelector("i");
+    icon.classList.toggle("fa-eye");
+    icon.classList.toggle("fa-eye-slash");
+}
+
+function autoCompleteSearch() {
+    const input  = document.getElementById("searchUser");
+    const list   = document.getElementById("autocomplete");
+    const button = document.querySelector('.btn-navbar');
+    list.style.right = `${button.offsetWidth}px`;
+    list.style.width = `${input.offsetWidth + 10}px`;
+
+    input.addEventListener("input", async function () {
+        const query = this.value.trim();
+        list.innerHTML = "";
+        if (query.length === 0) {
+            return;
+        }
+
+        try {
+            const response = await fetch(`/webui/autocompletion?q=${encodeURIComponent(query)}`);    
+            const names = await response.json();
+
+            names.forEach(name => {
+                const li = document.createElement("li");
+                li.className = "list-group-item list-group-item-action";
+                li.textContent = name;
+                li.onclick = () => {
+                    input.value = name;
+                    list.classList.add("d-none");
+                };
+                list.appendChild(li);
+            });
+
+            list.classList.toggle("d-none", names.length === 0);
+        } catch {}
+    });
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     filterPermissions();
     tooltip();
+    autoCompleteSearch();
 });
