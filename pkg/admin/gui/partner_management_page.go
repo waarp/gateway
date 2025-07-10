20c49a73f8a76a97368d5d4403b354e3d8ef38d3
Rayan Mazri <rayan.mazri@waarp.org>
2025-07-10 10:52:57 +0200

    feat/add popUp error for method add, edit, delete. Change method get -> post for add, edit, delete

--- pkg/admin/gui/partner_management_page.go
+++ pkg/admin/gui/partner_management_page.go
@@ -72,7 +72,6 @@ func editPartner(db *database.DB, r *http.Request) error {
 
 func addPartner(db *database.DB, r *http.Request) error {
 	var newPartner model.RemoteAgent
-	urlParams := r.URL.Query()
 
 	if err := r.ParseForm(); err != nil {
 		return fmt.Errorf("failed to parse form: %w", err)
