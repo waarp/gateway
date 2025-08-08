7a3c77bc63813e213485565f1c562614d86c17c4
Rayan Mazri <rayan.mazri@waarp.org>
2025-08-08 23:08:45 +0100

    feat/add last feature transfer monitoring page (and add recommendation last MR)

--- pkg/admin/gui/protocol.go
+++ pkg/admin/gui/protocol.go
@@ -24,8 +24,6 @@ type Protocols struct {
 	PeSITTLS string
 }
 
-const PreConnectionAuth = "pesit_pre-connection_auth"
-
 func supportedProtocolInternal(protocol string) []string {
 	supportedProtocolsInternal := map[string][]string{
 		r66.R66:         {auth.Password},
